e285266ce949d4fbed168ba5a4ad3002697d29e3
Muhsin-42 <muhsinofficial21@gmail.com>
2023-06-26 13:15:10 +0530

    structured redis code

--- server/src/utils/redis.ts
+++ server/src/utils/redis.ts
@@ -1,6 +1,5 @@
 import { createClient } from 'redis';
 
-
 const redisUrl = 'redis://127.0.0.1:6379';
 const redisClient = createClient({ url: redisUrl });
 (async () => {
@@ -14,7 +13,6 @@ redisClient.on('reconnecting', () => console.log('Cache is reconnecting'));
 redisClient.on('error', (e) => console.log(e));
 
 export default redisClient;
-
 
 export const redisGetAsync = (redisClient.get).bind(redisClient);
 export const redisSetAsync = (redisClient.set).bind(redisClient);
