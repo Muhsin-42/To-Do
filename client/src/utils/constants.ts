a60da90fca2b3a0635e5a12eae95aa2f60da9492
Muhsin-42 <muhsinofficial21@gmail.com>
2023-06-26 14:52:34 +0530

    updated constant urls

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,2 +1,3 @@
 export const serverBaseUrl = import.meta.env.VITE_SERVER_BASE_URL
 export const todoUrl = `${serverBaseUrl}api/todo`;
+export const todoSearchUrl = `${serverBaseUrl}api/todo/search`;
